42308882892983204515299ff29552f2ac7a9ac7
pratikkendale <157377217+pratikkendale@users.noreply.github.com>
2024-08-21 22:25:00 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,95 @@
+let gameseq = [];
+let userseq = [];
+
+
+let btns=["yellow" ,"red","purple","green"]
+
+let started = false;
+let level =0 ;
+
+let h2=document.querySelector("h2");
+
+document.addEventListener("keypress",function(){
+    if(started==false){
+        console.log("Game is started");
+        started=true; 
+
+        levelUp();
+    }
+});
+
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },300);
+}
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },300);
+}
+
+
+
+function levelUp(){
+    userseq=[];
+    level++;
+    h2.innerText=`Level ${level}`;
+
+    let randIdx=Math.floor(Math.random()*3);
+    let randColor=btns[randIdx];
+    let randBtn=document.querySelector(`.${randColor}`)
+    // console.log(randColor);
+    // console.log(randBtn);
+    // console.log(randIdx);
+    gameseq.push(randColor);
+    console.log(gameseq);
+    gameFlash(randBtn);
+} 
+
+function checkAns(idx){
+   
+
+    if(userseq[idx] === gameseq[idx]){
+        if(userseq.length==gameseq.length){
+            setTimeout(levelUp,1000);
+        }
+    }
+    else{
+        h2.innerHTML=`Game over! Your Score was ${level} <br>  Press any key to start`;
+        document.querySelector("body").style.backgroundColor="red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor="white";
+        },250)
+      
+        reset();
+        }
+}
+
+
+function btnPress() {
+   let btn=this;
+   userFlash(btn);
+
+   userColor = btn.getAttribute("id");
+   userseq.push(userColor);
+
+   checkAns(userseq.length-1);
+}
+
+let allBtns=document.querySelectorAll(".btn");
+for (btn of allBtns){
+    btn.addEventListener("click",btnPress);
+}
+
+
+
+function reset(){
+    started=false;
+    gameseq=[];
+    userseq=[];
+    level=0;
+}
